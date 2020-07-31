1fa44934f64f09852c4da781bc672930baa1f11b
GneccoFR <GneccoFR@gmail.com>
2020-07-31 01:20:26 -0300

    Implemented Animations
    Restructured Menu scripting to be controlled by only one GameObject and added animations to Intro Game, Open and exit Main Menu an Open and Exit Options Menu. 
    Known bugs: transition from pause menu to Main Menu not working.

---
+++ Assets/Scripts/CanvasScript.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CanvasScript : MonoBehaviour
+{
+    public GameManager manager;
+    public GameObject optionsMenu;
+    public GameObject mainMenu;
+    Animator animator;
+    public bool mainMenuIsActive;
+
+    void Awake()
+    {
+        animator = GetComponent<Animator>();
+        animator.SetTrigger("MainOpen");
+        mainMenuIsActive = true;
+        manager = GameManager.GetInstance();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) && mainMenuIsActive == false) 
+            SwapMenus();
+    }
+
+    public void SwapMenus()
+    {
+        if (mainMenuIsActive == true)
+        {
+            animator.SetTrigger("MainClose");
+            animator.SetTrigger("OptOpen");
+        }
+        else
+        {
+            animator.SetTrigger("OptClose");
+            animator.SetTrigger("MainOpen");
+        }
+        mainMenuIsActive = !mainMenuIsActive;
+    }
+
+    public void LoadGame()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene("Main");
+        animator.SetTrigger("MainClose");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
